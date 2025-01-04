125e9668ab47780229cf52010d9ac6702693215f
Virendranath Yadav <virendranathyadav@Virendranaths-MacBook-Pro.local>
2025-01-04 13:20:11 +0530

    Updated login testing file

--- frontend/notes-app/src/pages/Login/login.jsx
+++ frontend/notes-app/src/pages/Login/login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Navbar from '../../components/Navbar/Navbar'
+//import Navbar from '../../components/Navbar/Navbar'
 import {Link, useNavigate} from 'react-router-dom'
 import PasswordInput from '../../components/Input/PasswordInput'
 import { validateEmail } from '../../utils/helper'
--- frontend/notes-app/src/tests/components/login.test.jsx
+++ frontend/notes-app/src/tests/components/login.test.jsx
@@ -2,12 +2,35 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import { it, describe } from "vitest";
 import Login from "../../pages/Login/login";
+import { MemoryRouter as Router, Routes, Route } from "react-router-dom";
 
+const renderLoginPage = () => {
+    render(
+            <Router initialEntries={['/login']}>
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                </Routes>
+            </Router>
+    );
+};
 
 describe('Login component', () => {
-    it('should test login component', () => {
-        render(<Login Email="testuser@gmail.com" password="testuser@123" />);
+    it("validate function should pass on correct input ", () => {
+        /*render(<Login />);
+        const inputNode = screen.getByPlaceholderText('Email');
+        <Router>
+                <Login />
+        </Router>
+        screen.debug();*/
+        renderLoginPage();
+        const emailInput = screen.getByPlaceholderText('Email:');
+        const passwordInput = screen.getByPlaceholderText('Password:');
 
-        screen.debug();
+        fireEvent.change(emailInput, { target: { value: 'testuser@gmail.com' } });
+        fireEvent.change(passwordInput, { target: { value: 'testuser@123' } });
+
+        expect(emailInput.value).toBe('testuser@gmail.com');
+        expect(passwordInput.value).toBe('testuser@123');
+
     });
 });

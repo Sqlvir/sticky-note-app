7a51570aa22b6617335f06589a84af375c8b7b99
Virendranath <virendranathyadav81@gmail.com>
2025-03-01 13:03:14 +0530

    Created new end to end cypress testing file

--- frontend/notes-app/cypress/e2e/end-to-end.cy.js
+++ frontend/notes-app/cypress/e2e/end-to-end.cy.js
@@ -1,23 +1,32 @@
 describe('Sticky note end to end test', () => {
+  
+  it('shows error message with invalid credentials and then registers new user', () => {
+    // Test invalid login
+    cy.visit('http://localhost:8080/login')
+    cy.get('input[type="text"]').type('wrong@example.com')
+    cy.get('input[type="password"]').type('wrongpassword')
+    cy.get('button[type="submit"]').click()
+    
+    cy.contains('User not found').should('be.visible')
+  })
+ 
+  it('Register new user', () => {
+    // Navigate to registration
+    cy.visit('http://localhost:8080/signup')
 
-  // Verify successful login and redirect to sticky note page
+    // Fill registration form
+    cy.get('input[placeholder="Name"]').type('Sunny Kumar')
+    cy.get('input[placeholder="Email"]').type('sunnykumar@gmail.com')
+    cy.get('input[placeholder="Password"]').type('password123')
+    cy.get('.register-button').click()
+  })
+
   it('successfully logs in with valid credentials', () => {
     cy.visit('http://localhost:8080/login')
-    cy.get('input[type="text"]').type('testuser@gmail.com')
-    cy.get('input[type="password"]').type('testuser@123')
+    cy.get('input[type="text"]').type('sunnykumar@gmail.com')
+    cy.get('input[type="password"]').type('password123')
     cy.get('button[type="submit"]').click()
     
     cy.url().should('eq', 'http://localhost:8080/stickynote')
   })
-
-  // Verify unsuccessful login with invalid credentials
-  // it('shows error message with invalid credentials', () => {
-  //   cy.visit('http://localhost:8080/login')
-  //   cy.get('input[type="text"]').type('wrong@example.com')
-  //   cy.get('input[type="password"]').type('wrongpassword')
-  //   cy.get('button[type="submit"]').click()
-    
-  //   cy.contains('User not found').should('be.visible')
-  //   cy.url().should('include', '/login')
-  // })
 })
